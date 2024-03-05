c835a6aa2e4f1d65bb345f3d17785765f9b4f0c9
luwed <lukasz4321@hotmail.com>
2024-03-05 12:58:48 +0100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,15 +9,18 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});  
+});
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
