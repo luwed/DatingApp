f8f3199490a9a50a6ecd470e95a34b5724568117
luwed <lukasz4321@hotmail.com>
2024-03-07 11:32:53 +0100

    End of section 7

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Errors
+{
+    public class ApiException
+    {
+        public ApiException(int statsuCode, string message, string details)
+        {
+            StatsuCode = statsuCode;
+            Message = message;
+            Details = details;
+        }
+
+        public int StatsuCode { get; set; }
+
+        public string Message { get; set; }
+
+        public string Details { get; set; }
+    }
+}
