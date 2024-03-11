0b575d668ece165bc67801691f47d323e5fdae44
luwed <lukasz4321@hotmail.com>
2024-03-11 13:13:45 +0100

    End of section 13

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -27,12 +28,22 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers() 
+        public async Task<ActionResult<PagedList<MemberDto>>> GetUsers([FromQuery]UserParams userParams) 
         {
-            var users = await _userRepository.GetMembersAsync();
+            var currentUser = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            userParams.CurrentUsername = currentUser.UserName;
+
+            if (string.IsNullOrEmpty(userParams.Gender))
+            {
+                userParams.Gender = currentUser.Gender == "male" ? "female" : "male";
+            }
+
+            var users = await _userRepository.GetMembersAsync(userParams);
+
+            Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages));
+
             return Ok(users);
         }
-
 
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
@@ -43,7 +54,7 @@ namespace API.Controllers
         [HttpPut]
         public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
         {
-            var user = await _userRepository.GetUserByUsernameAsync(User.GeetUsername());
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
             if (user == null) 
             {
                 return NotFound();
@@ -62,7 +73,7 @@ namespace API.Controllers
         [HttpPost("add-photo")]
         public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
         {
-            var user = await _userRepository.GetUserByUsernameAsync(User.GeetUsername());
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
 
             if (user == null) 
             {
@@ -99,7 +110,7 @@ namespace API.Controllers
         [HttpPut("set-main-photo/{photoid}")]
         public async Task<ActionResult> UpdateSetMainPhotoUser(int photoid)
         {
-            var user = await _userRepository.GetUserByUsernameAsync(User.GeetUsername());
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
             if (user == null)
             {
                 return NotFound();
@@ -135,7 +146,7 @@ namespace API.Controllers
         [HttpDelete("delete-photo/{photoid}")]
         public async Task<ActionResult> DeletePhoto(int photoId)
         {
-            var user = await _userRepository.GetUserByUsernameAsync(User.GeetUsername());
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
             
             var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
             if (photo == null)
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,8 +1,12 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
-import { map, of } from 'rxjs';
+import { map, of, take } from 'rxjs';
+import { PaginatedResult } from '../_modules/pagination';
+import { UserParams } from '../_models/userParams';
+import { AccountService } from './account.service';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -10,27 +14,69 @@ import { map, of } from 'rxjs';
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  memberCache = new Map();
+  userParams: UserParams | undefined;
+  user: User | undefined;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => {
+        if (user) {
+          this.userParams = new UserParams(user);
+          this.user = user;
+        }
+      }
+    });
+  }
 
-  getMembers() {
-    if (this.members.length > 0) {
-      return of(this.members);
+  getUserParams() {
+    return this.userParams;
+  }
+
+  setUserParams(params: UserParams) {
+    this.userParams = params;
+  }
+
+  resetUserParams() {
+    if (this.user) {
+      this.userParams = new UserParams(this.user);
+      return this.userParams;
     }
-    // return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
-    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
-      map(members => {
-        this.members = members;
-        return this.members;
+
+    return;
+  }
+
+  getMembers(userParams: UserParams) {
+    const response = this.memberCache.get(Object.values(userParams).join('-'));
+
+    if (response) {
+      return of(response);
+    }
+
+    let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
+
+    params = params.append("minAge", userParams.minAge);
+    params = params.append("maxAge", userParams.maxAge);
+    params = params.append("gender", userParams.gender);
+    params = params.append("orderBy", userParams.orderBy);
+
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params).pipe(
+      map(response => {
+        this.memberCache.set(Object.values(userParams).join('-'), response);
+        return response;
       })
     );
   }
 
   getMember(username: string) {
-    const member = this.members.find(x => x.userName === username);
+    const member = [...this.memberCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.result), [])
+      .find((member: Member) => member.userName === username);
+
     if (member) {
       return of(member);
     }
+
     // return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions());
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
@@ -39,7 +85,7 @@ export class MembersService {
     return this.http.put(this.baseUrl + 'users', member).pipe(
       map(() => {
         const index = this.members.indexOf(member);
-        this.members[index] = {...this.members[index], ...member}
+        this.members[index] = { ...this.members[index], ...member }
       })
     );
   }
@@ -50,19 +96,46 @@ export class MembersService {
 
   deletePhoto(photoId: number) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
-  }  
+  }
 
-//   getHttpOptions() {
-//     const userString = localStorage.getItem('user');
-//     if (!userString) {
-//       return;
-//     }
+  private getPaginatedResult<T>(url: string, params: HttpParams) {
+    const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>;
+    return this.http.get<T>(url, { observe: 'response', params }).pipe(
+      map(response => {
+        if (response.body) {
+          paginatedResult.result = response.body;
+        }
 
-//     const user = JSON.parse(userString);
-//     return {
-//       headers: new HttpHeaders({
-//         Authorization: 'Bearer ' + user.token
-//       })
-//     }
-//   }
+        const pagination = response.headers.get('Pagination');
+        if (pagination) {
+          paginatedResult.pagination = JSON.parse(pagination);
+        }
+
+        return paginatedResult;
+      })
+    );
+  }
+
+  private getPaginationHeaders(pageNumber: number, pageSize: number) {
+    let params = new HttpParams();
+
+    params = params.append('pageNumber', pageNumber);
+    params = params.append('pageSize', pageSize);
+
+    return params;
+  }
+
+  //   getHttpOptions() {
+  //     const userString = localStorage.getItem('user');
+  //     if (!userString) {
+  //       return;
+  //     }
+
+  //     const user = JSON.parse(userString);
+  //     return {
+  //       headers: new HttpHeaders({
+  //         Authorization: 'Bearer ' + user.token
+  //       })
+  //     }
+  //   }
 }
